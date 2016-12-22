3a7e73be901b790d40f37f7dc055a01efba75016
Arien Chen <arien.chen@gmail.com>
2016-12-22 16:13:05 +0800

    Create test-api.py

---
+++ test/python/test-api.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from tibrv.api import *
+from tibrv.status import *
+import unittest
+
+
+class VersionTest(unittest.TestCase):
+
+    def setUp(self):
+        status = Tibrv.open()
+        self.assertEqual(TIBRV_OK, status, TibrvStatus.text(status))
+
+    def tearDown(self):
+        status = Tibrv.close()
+        self.assertEqual(TIBRV_OK, status, TibrvStatus.text(status))
+
+    def test_version(self):
+
+        print('')
+        ver = Tibrv.version()
+        print('TIBRV VERSION:', ver)
+        self.assertIsNotNone(ver)
+        #self.assertEqual('8.4.5', ver)
+
+if __name__ == "__main__" :
+   unittest.main(verbosity=2)
