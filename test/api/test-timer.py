74c4eb0da1e1174cdaf842026abeab50069477bb
Arien Chen <arien.chen@fubon.com>
2016-12-23 16:45:16 +0800

    porting from test/python/test-timer.py

---
+++ test/api/test-timer.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+import time
+from pytibrv.tport import *
+from pytibrv.status import *
+from pytibrv.tport import *
+from pytibrv.events import *
+from pytibrv.disp import *
+import unittest
+
+
+def callback(event:tibrvEvent, message:tibrvMsg, closure):
+
+    test = tibrvClosure(closure)
+    test.counter += 1
+    print(test.counter, datetime.now())
+    if test.counter >= 10:
+        status = tibrvEvent_Destroy(event)
+        if TIBRV_OK != status:
+            raise TibrvError(status)
+        test.tm = 0
+
+class TimerTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        status = Tibrv.open()
+        if status != TIBRV_OK:
+            raise TibrvError(status)
+
+    @classmethod
+    def tearDownClass(cls):
+        Tibrv.close()
+
+
+    def test_create(self):
+
+        self.counter = 0
+
+        status, que = tibrvQueue_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, disp = tibrvDispatcher_Create(que)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        print('')
+
+        # pass self as closure
+        # self.tm will assign to 0 when self.counter >= 0
+        status, tm = tibrvEvent_CreateTimer(que, callback, 1.0, self)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        self.tm = tm
+
+        # run for 11 seconds
+        timeout = time.time() + 11
+
+        while time.time() <= timeout:
+            time.sleep(0.5)
+            # wait till callback() destroy iteself when counter >= 10
+            if self.tm == 0:
+                break;
+            #print('SLEEP...')
+
+        # timer should be destroyed in callback()
+        status = tibrvEvent_Destroy(tm)
+        self.assertEqual(TIBRV_INVALID_EVENT, status, tibrvStatus_GetText(status))
+
+        self.assertEqual(10, self.counter)
+
+        status = tibrvQueue_Destroy(que)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        print('TEST DONE')
+
+if __name__ == "__main__" :
+    unittest.main(verbosity=2)
