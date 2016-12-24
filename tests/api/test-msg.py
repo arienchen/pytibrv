8cba9ec1a6acaf6ddfb2410e14a5f37e3e17121f
Arien Chen <arien.chen@fubon.com>
2016-12-24 18:46:04 +0800

    revise import

--- tests/api/test-msg.py
+++ tests/api/test-msg.py
@@ -1,5 +1,8 @@
 
 import ctypes
+
+from pytibrv.api import *
+from pytibrv.status import *
 from pytibrv.msg import *
 import unittest
 
@@ -7,261 +10,315 @@ class MsgTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        status = Tibrv.open()
-        assert TIBRV_OK == status, TibrvStatus.text(status)
+        status = tibrv_Open()
+        assert TIBRV_OK == status, tibrvStatus_GetText(status)
 
     @classmethod
     def tearDownClass(cls):
-        status = Tibrv.close()
+        tibrv_Close()
 
     def test_new(self):
-        # Default Constructor
-        msg = TibrvMsg()
-        sz = str(msg)
+
+        status, msg = tibrvMsg_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, sz = tibrvMsg_ConvertToString(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
         self.assertEqual("{}", sz)
 
-        m = msg._msg
-
-        del msg
-
-        # tibrvMsg_Destroy() should be called at __del__
-        status = tibrvMsg_Destroy(m)
-        self.assertEqual(TIBRV_INVALID_MSG, status, TibrvStatus.text(status))
+        status = tibrvMsg_Destroy(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
     def test_copy(self):
-        msg = TibrvMsg()
-        msg.setStr('A', 'TEST')
-        m = msg.id()
-        msg2 = msg.copy()
-        # msg content must be same
-        self.assertEqual(str(msg), str(msg2))
 
-        # enable exception
-        TibrvStatus.exception(True)
-        try:
-            del msg
-            del msg2
-        except:
-            self.fail('MUST NO EXCEPTION')
+        status, msg = tibrvMsg_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-        # disable exception
-        TibrvStatus.exception(False)
+        status = tibrvMsg_UpdateString(msg, 'A', 'TEST')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, msg2 = tibrvMsg_CreateCopy(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, sz = tibrvMsg_ConvertToString(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, sz2 = tibrvMsg_ConvertToString(msg2)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        self.assertEqual(sz, sz2)
+
+        status = tibrvMsg_Destroy(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status = tibrvMsg_Destroy(msg2)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
     def test_invalid(self):
 
-        msg = TibrvMsg()
-        m = msg.id()
-        del msg
+        status, msg = tibrvMsg_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status = tibrvMsg_Destroy(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
         # construct by invalid msg id, which just destroyed
-        msg = TibrvMsg(m)
-        msg.sendSubject = 'TEST'
-        status = msg.error.code()
+        status = tibrvMsg_SetSendSubject(msg, 'TEST')
+        self.assertEqual(TIBRV_INVALID_MSG, status, tibrvStatus_GetText(status))
 
-        self.assertEqual(TIBRV_INVALID_MSG, status, TibrvStatus.text(status))
+        status = tibrvMsg_Destroy(msg)
+        self.assertEqual(TIBRV_INVALID_MSG, status, tibrvStatus_GetText(status))
 
-        del msg
 
         # assign random msg id, ex: 12345
-        # msg = TibrvMsg(12345)
         # DONT TRY IT, SEGMENT FAULT
         #
+        #status = tibrvMsg_Destroy(12345)
 
     def test_subject(self):
-        msg = TibrvMsg()
-        msg.sendSubject = 'TEST'
-        self.assertEqual('TEST', msg.sendSubject)\
+        status, msg = tibrvMsg_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-        msg.replySubject = 'TEST2'
-        self.assertEqual('TEST2', msg.replySubject)\
+        status = tibrvMsg_SetSendSubject(msg, 'TEST')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-        del msg
+        status, subj = tibrvMsg_GetSendSubject(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        self.assertEqual('TEST', subj)
+
+        status = tibrvMsg_SetReplySubject(msg, 'TEST2')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, subj = tibrvMsg_GetReplySubject(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual('TEST2', subj)
+
+        status = tibrvMsg_Destroy(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
 
     def test_get(self):
-        msg = TibrvMsg()
+        status, msg = tibrvMsg_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-        msg.setI8('I8', 0xFFFF)
-        self.assertEqual(-1, msg.getI8('I8'))
-        self.assertEqual(-1, msg.get(tibrv_i8, 'I8'))
+        status = tibrvMsg_UpdateI8(msg, 'I8', 0xFFFF)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetI8(msg, 'I8')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(-1, n)
 
-        msg.setU8('U8', 0xFFFF)
-        self.assertEqual(0xFF, msg.getU8('U8'))
-        self.assertEqual(0xFF, msg.get(tibrv_u8, 'U8'))
+        status = tibrvMsg_UpdateU8(msg, 'U8', 0xFFFF)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetU8(msg, 'U8')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(0x00FF, n)
 
-        msg.setI16('I16', 0xFFFFFFFE)
-        self.assertEqual(-2, msg.getI16('I16'))
-        self.assertEqual(-2, msg.get(tibrv_i16, 'I16'))
+        status = tibrvMsg_UpdateI16(msg, 'I16', 0xFFFFFFFE)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetI16(msg, 'I16')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(-2, n)
 
-        msg.setU16('U16', 0xFFFFFFFE)
-        self.assertEqual(0xFFFE, msg.getU16('U16'))
-        self.assertEqual(0xFFFE, msg.get(tibrv_u16, 'U16'))
+        status = tibrvMsg_UpdateU16(msg, 'U16', 0xFFFFFFFE)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetU16(msg, 'U16')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(0x00FFFE, n)
 
-        msg.setI32('I32', 0x0000FFFFFFFFFFFD)
-        self.assertEqual(-3, msg.getI32('I32'))
-        self.assertEqual(-3, msg.get(tibrv_i32, 'I32'))
+        status = tibrvMsg_UpdateI32(msg, 'I32', 0x0000FFFFFFFFFFFD)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetI32(msg, 'I32')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(-3, n)
 
-        msg.setU32('U32', 0x0000FFFFFFFFFFFD)
-        self.assertEqual(0xFFFFFFFD, msg.getU32('U32'))
-        self.assertEqual(0xFFFFFFFD, msg.get(tibrv_u32, 'U32'))
+        status = tibrvMsg_UpdateU32(msg, 'U32', 0x0000FFFFFFFFFFFD)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetU32(msg, 'U32')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(0x00FFFFFFFD, n)
 
-        msg.setI64('I64', 0xfffffffffffffffc)
-        self.assertEqual(-4, msg.getI64('I64'))
-        self.assertEqual(-4, msg.get(tibrv_i64, 'I64'))
+        status = tibrvMsg_UpdateI64(msg, 'I64', 0xfffffffffffffffc)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetI64(msg, 'I64')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(-4, n)
 
-        msg.setU64('U64', 0xFFFFFFFFFFFFFFFC)
-        self.assertEqual(0xFFFFFFFFFFFFFFFC, msg.getU64('U64'))
-        self.assertEqual(0xFFFFFFFFFFFFFFFC, msg.get(tibrv_u64, 'U64'))
+        status = tibrvMsg_UpdateU64(msg, 'U64', 0xFFFFFFFFFFFFFFFC)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, n = tibrvMsg_GetU64(msg, 'U64')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(0x00FFFFFFFFFFFFFFFC, n)
 
-        msg.setStr('STR', 'TEST')
-        self.assertEqual('TEST', msg.getStr('STR'))
-        self.assertEqual('TEST', msg.get(tibrv_str, 'STR'))
+        status = tibrvMsg_UpdateString(msg, 'STR', 'TEST')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status, sz = tibrvMsg_GetString(msg, 'STR')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual('TEST', sz)
 
-        msg2 = TibrvMsg()
-        msg2.set(tibrv_str, 'DATA', 'TEST')
-        msg.setMsg('MSG', msg2)
-        msgx = msg.getMsg('MSG')
-        self.assertEqual(str(msg2), str(msgx))
-        msgx = msg.get(TibrvMsg, 'MSG')
-        self.assertEqual(str(msg2), str(msgx))
+        status, msg2 = tibrvMsg_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        status = tibrvMsg_UpdateString(msg2, 'DATA', 'TEST')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
+        status = tibrvMsg_UpdateMsg(msg, 'MSG', msg2)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-        del msg
+        status, mm = tibrvMsg_GetMsg(msg, 'MSG')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, sz = tibrvMsg_ConvertToString(msg2)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, sz2 = tibrvMsg_ConvertToString(mm)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        self.assertEqual(sz, sz2)
+
+        status = tibrvMsg_Destroy(msg)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status = tibrvMsg_Destroy(msg2)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
 
     def test_datetime(self):
 
         status, msg = tibrvMsg_Create()
-        assert status == TIBRV_OK, TibrvStatus.text(status)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
         status = tibrvMsg_AddDateTime(msg, 'DT', None)
-        assert status == TIBRV_OK, TibrvStatus.text(status)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
         status = tibrvMsg_UpdateDateTime(msg, 'DT', None)
-        assert status == TIBRV_OK, TibrvStatus.text(status)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
         status, ret = tibrvMsg_GetDateTime(msg, 'DT')
-        assert status == TIBRV_OK, TibrvStatus.text(status)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
         assert ret is not None
+
+        status, dt = tibrvMsg_GetCurrentTime()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertTrue(type(dt) is tibrvMsgDateTime)
+        print(dt)
+
+        status = tibrvMsg_UpdateDateTime(msg, 'DT', dt)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, dt2 = tibrvMsg_GetDateTime(msg, 'DT')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertTrue(type(dt2) is tibrvMsgDateTime)
+        self.assertEqual(dt, dt2)
+
+        dt3 = tibrvMsgDateTime()
+        status = tibrvMsg_UpdateDateTime(msg, 'DT3', dt3)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+
+        status, dt4 = tibrvMsg_GetDateTime(msg, 'DT3')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        self.assertEqual(dt3, dt4)
 
         status = tibrvMsg_Destroy(msg)
-        assert status == TIBRV_OK, TibrvStatus.text(status)
-
-        msg = TibrvMsg()
-        dt = TibrvMsg.now()
-
-        status = msg.addDateTime('DT', dt)
-        assert status == TIBRV_OK, TibrvStatus.text(status)
-
-        status = msg.setDateTime('DT', dt)
-        assert status == TIBRV_OK, TibrvStatus.text(status)
-
-        ret = msg.getDateTime('DT')
-        assert ret is not None
-        # print(ret, dt)
-        assert ret == dt, str(ret) + ' != ' + str(dt)
-
-        del msg
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
 
+    def test_array(self):
 
-def test_array():
+        status, msg = tibrvMsg_Create()
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    status = TIBRV_OK
-    status, msg = tibrvMsg_Create()
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        # I8
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateI8Array(msg, 'I8', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # I8
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateI8Array(msg, 'I8', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetI8Array(msg, 'I8')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetI8Array(msg, 'I8')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # U8
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateU8Array(msg, 'U8', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # U8
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateU8Array(msg, 'U8', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetU8Array(msg, 'U8')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetU8Array(msg, 'U8')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # I16
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateI16Array(msg, 'I16', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # I16
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateI16Array(msg, 'I16', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetI16Array(msg, 'I16')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetI16Array(msg, 'I16')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # U16
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateU16Array(msg, 'U16', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # U16
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateU16Array(msg, 'U16', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetU16Array(msg, 'U16')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetU16Array(msg, 'U16')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # I32
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateI32Array(msg, 'I32', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # I32
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateI32Array(msg, 'I32', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetI32Array(msg, 'I32')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetI32Array(msg, 'I32')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # U32
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateU32Array(msg, 'U32', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # U32
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateU32Array(msg, 'U32', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetU32Array(msg, 'U32')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetU32Array(msg, 'U32')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # I64
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateI64Array(msg, 'I64', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # I64
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateI64Array(msg, 'I64', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetI32Array(msg, 'I64')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetI32Array(msg, 'I64')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # U64
+        data = [1,2,3,4,5]
+        status = tibrvMsg_UpdateU64Array(msg, 'U64', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # U64
-    data = [1,2,3,4,5]
-    status = tibrvMsg_UpdateU64Array(msg, 'U64', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetU64Array(msg, 'U64')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
-    status, ret = tibrvMsg_GetU64Array(msg, 'U64')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        # F32
+        data = [1.1,2.2,3.3,4.4,5.5]
+        status = tibrvMsg_UpdateF32Array(msg, 'F32', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # F32
-    data = [1.1,2.2,3.3,4.4,5.5]
-    status = tibrvMsg_UpdateF32Array(msg, 'F32', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
+        status, ret = tibrvMsg_GetF32Array(msg, 'F32')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        for x in range(len(data)):
+            f = ctypes.c_float(data[x]).value   # convert to F32
+            assert f == ret[x]
 
-    status, ret = tibrvMsg_GetF32Array(msg, 'F32')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    for x in range(len(data)):
-        f = ctypes.c_float(data[x]).value   # convert to F32
-        assert f == ret[x]
+        # F64
+        data = [1.1,2.2,3.3,4.4,5.5]
+        status = tibrvMsg_UpdateF64Array(msg, 'F64', data)
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
 
-    # F64
-    data = [1.1,2.2,3.3,4.4,5.5]
-    status = tibrvMsg_UpdateF64Array(msg, 'F64', data)
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-
-    status, ret = tibrvMsg_GetF64Array(msg, 'F64')
-    assert status == TIBRV_OK, TibrvStatus.text(status)
-    assert data == ret
+        status, ret = tibrvMsg_GetF64Array(msg, 'F64')
+        self.assertEqual(TIBRV_OK, status, tibrvStatus_GetText(status))
+        assert data == ret
 
 
 if __name__ == "__main__":
