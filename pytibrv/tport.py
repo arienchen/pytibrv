0823479edc62b5d824f544f78ce4f93c37acdbf7
Arien Chen <arien.chen@fubon.com>
2016-12-24 13:42:26 +0800

    move class TibrvTx to Tibrv.py

--- pytibrv/tport.py
+++ pytibrv/tport.py
@@ -6,11 +6,11 @@
 # LAST MODIFIED : V1.0 20161211 ARIEN arien.chen@gmail.com 
 #
 # DESCRIPTIONS
-# ---------------------------------------------------
+##-----------------------------------------------------------------------------
 #
 #
 # FEATURES: * = un-implement
-# ------------------------------------------------------
+##-----------------------------------------------------------------------------
 #   tibrvTransport_Create
 #   tibrvTransport_CreateInbox
 #   tibrvTransport_Destroy
@@ -34,24 +34,22 @@
 #  *tibrvTransport_SetBatchSize
 #  *tibrvTransport_CreateLicensed
 #
-# Python Class 
-# ------------------------------------------------------
-#   TibrvTx 
-# 
+#
 # CHANGED LOGS
-# ---------------------------------------------------
+##-----------------------------------------------------------------------------
 # 20161211 V1.0 ARIEN arien.chen@gmail.com
 #   CREATED
 #
 ##
 
 import ctypes as _ctypes
-from .msg import *
-from .api import _rv, _cstr, _pystr
+from .types import tibrv_status, tibrvTransport, tibrvMsg, \
+                   TIBRV_SUBJECT_MAX
 
-##-----------------------------------------------------------------------------
-## tibrvTransport
-##-----------------------------------------------------------------------------
+from .api import _rv, _cstr, _pystr, \
+                 _c_tibrvTransport, _c_tibrvMsg, \
+                 _c_tibrv_status, _c_tibrv_u32, _c_tibrv_f64
+
 
 # tibrv/tport.h
 # tibrv_status tibrvTransport_Create(
@@ -60,15 +58,15 @@ from .api import _rv, _cstr, _pystr
 #                const char*         network,
 #                const char*         daemonStr
 #              );
-_rv.tibrvTransport_Create.argtypes = [_ctypes.POINTER(c_tibrvTransport),
+_rv.tibrvTransport_Create.argtypes = [_ctypes.POINTER(_c_tibrvTransport),
                                       _ctypes.c_char_p,
                                       _ctypes.c_char_p,
                                       _ctypes.c_char_p]
-_rv.tibrvTransport_Create.restype = c_tibrv_status
+_rv.tibrvTransport_Create.restype = _c_tibrv_status
 
 def tibrvTransport_Create(service: str, network: str, daemon: str) -> (tibrv_status, tibrvTransport):
 
-    tx = c_tibrvTransport(0)
+    tx = _c_tibrvTransport(0)
 
     status = _rv.tibrvTransport_Create(_ctypes.byref(tx), _cstr(service), _cstr(network), _cstr(daemon))
 
@@ -82,14 +80,14 @@ def tibrvTransport_Create(service: str, network: str, daemon: str) -> (tibrv_sta
 #                tibrvMsg            message
 #              );
 #
-_rv.tibrvTransport_Send.argtypes = [c_tibrvTransport, c_tibrvMsg]
-_rv.tibrvTransport_Send.restype = c_tibrv_status
+_rv.tibrvTransport_Send.argtypes = [_c_tibrvTransport, _c_tibrvMsg]
+_rv.tibrvTransport_Send.restype = _c_tibrv_status
 
 
 def tibrvTransport_Send(transport: tibrvTransport, message: tibrvMsg) -> tibrv_status:
 
-    tx = c_tibrvTransport(transport)
-    msg = c_tibrvMsg(message)
+    tx = _c_tibrvTransport(transport)
+    msg = _c_tibrvMsg(message)
 
     status = _rv.tibrvTransport_Send(tx, msg)
 
@@ -104,16 +102,20 @@ def tibrvTransport_Send(transport: tibrvTransport, message: tibrvMsg) -> tibrv_s
 #                tibrv_f64           idleTimeout
 #              );
 #
-_rv.tibrvTransport_SendRequest.argtypes = [c_tibrvTransport, c_tibrvMsg, _ctypes.POINTER(c_tibrvMsg), c_tibrv_f64]
-_rv.tibrvTransport_SendRequest.restype = c_tibrv_status
+_rv.tibrvTransport_SendRequest.argtypes = [_c_tibrvTransport,
+                                           _c_tibrvMsg,
+                                           _ctypes.POINTER(_c_tibrvMsg),
+                                           _c_tibrv_f64]
+_rv.tibrvTransport_SendRequest.restype = _c_tibrv_status
 
 
-def tibrvTransport_SendRequest(transport: tibrvTransport, message: tibrvMsg, idleTimeout: float) -> (tibrv_status, tibrvMsg):
+def tibrvTransport_SendRequest(transport: tibrvTransport, message: tibrvMsg, idleTimeout: float) \
+                              -> (tibrv_status, tibrvMsg):
 
-    tx = c_tibrvTransport(transport)
-    msg = c_tibrvMsg(message)
-    r = c_tibrvMsg(0)
-    t = c_tibrv_f64(idleTimeout)
+    tx = _c_tibrvTransport(transport)
+    msg = _c_tibrvMsg(message)
+    r = _c_tibrvMsg(0)
+    t = _c_tibrv_f64(idleTimeout)
 
     status = _rv.tibrvTransport_SendRequest(tx, msg, _ctypes.byref(r), t)
 
@@ -128,14 +130,15 @@ def tibrvTransport_SendRequest(transport: tibrvTransport, message: tibrvMsg, idl
 #                tibrvMsg            requestMessage
 #              );
 #
-_rv.tibrvTransport_SendReply.argtypes = [c_tibrvTransport, c_tibrvMsg, c_tibrvMsg]
-_rv.tibrvTransport_SendReply.restype = c_tibrv_status
+_rv.tibrvTransport_SendReply.argtypes = [_c_tibrvTransport, _c_tibrvMsg, _c_tibrvMsg]
+_rv.tibrvTransport_SendReply.restype = _c_tibrv_status
 
-def tibrvTransport_SendReply(transport: tibrvTransport, message: tibrvMsg, requestMessage: tibrvMsg) -> tibrv_status:
+def tibrvTransport_SendReply(transport: tibrvTransport, message: tibrvMsg, requestMessage: tibrvMsg) \
+                            -> tibrv_status:
 
-    tx = c_tibrvTransport(transport)
-    msg = c_tibrvMsg(message)
-    req = c_tibrvMsg(requestMessage)
+    tx = _c_tibrvTransport(transport)
+    msg = _c_tibrvMsg(message)
+    req = _c_tibrvMsg(requestMessage)
 
     status = _rv.tibrvTransport_SendReply(tx, msg, req)
 
@@ -148,13 +151,13 @@ def tibrvTransport_SendReply(transport: tibrvTransport, message: tibrvMsg, reque
 #                tibrvTransport      transport
 #              );
 #
-_rv.tibrvTransport_Destroy.argtypes = [c_tibrvTransport]
-_rv.tibrvTransport_Destroy.restype = c_tibrv_status
+_rv.tibrvTransport_Destroy.argtypes = [_c_tibrvTransport]
+_rv.tibrvTransport_Destroy.restype = _c_tibrv_status
 
 
 def tibrvTransport_Destroy(transport: tibrvTransport) -> tibrv_status:
 
-    tx = c_tibrvTransport(transport)
+    tx = _c_tibrvTransport(transport)
 
     status = _rv.tibrvTransport_Destroy(tx)
 
@@ -168,12 +171,12 @@ def tibrvTransport_Destroy(transport: tibrvTransport) -> tibrv_status:
 #                tibrv_u32           subjectLimit
 #              );
 #
-_rv.tibrvTransport_CreateInbox.argtypes = [c_tibrvTransport, _ctypes.c_char_p, c_tibrv_u32]
-_rv.tibrvTransport_CreateInbox.restype = c_tibrv_status
+_rv.tibrvTransport_CreateInbox.argtypes = [_c_tibrvTransport, _ctypes.c_char_p, _c_tibrv_u32]
+_rv.tibrvTransport_CreateInbox.restype = _c_tibrv_status
 
 def tibrvTransport_CreateInbox(transport: tibrvTransport) -> (tibrv_status, str):
 
-    tx = c_tibrvTransport(transport)
+    tx = _c_tibrvTransport(transport)
     subj = _ctypes.create_string_buffer(TIBRV_SUBJECT_MAX)
 
     status = _rv.tibrvTransport_CreateInbox(tx, subj, _ctypes.sizeof(subj))
@@ -188,12 +191,12 @@ def tibrvTransport_CreateInbox(transport: tibrvTransport) -> (tibrv_status, str)
 #                const char**        serviceString
 #              );
 #
-_rv.tibrvTransport_GetService.argtypes = [c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
-_rv.tibrvTransport_GetService.restype = c_tibrv_status
+_rv.tibrvTransport_GetService.argtypes = [_c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
+_rv.tibrvTransport_GetService.restype = _c_tibrv_status
 
 def tibrvTransport_GetService(transport: tibrvTransport) -> (tibrv_status, str):
 
-    tx = c_tibrvTransport(transport)
+    tx = _c_tibrvTransport(transport)
     sz = _ctypes.c_char_p(0)
     status = _rv.tibrvTransport_GetService(tx, _ctypes.byref(sz))
 
@@ -207,12 +210,12 @@ def tibrvTransport_GetService(transport: tibrvTransport) -> (tibrv_status, str):
 #                const char**        networkString
 #              );
 #
-_rv.tibrvTransport_GetNetwork.argtypes = [c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
-_rv.tibrvTransport_GetNetwork.restype = c_tibrv_status
+_rv.tibrvTransport_GetNetwork.argtypes = [_c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
+_rv.tibrvTransport_GetNetwork.restype = _c_tibrv_status
 
 def tibrvTransport_GetNetwork(transport: tibrvTransport) -> (tibrv_status, str):
 
-    tx = c_tibrvTransport(transport)
+    tx = _c_tibrvTransport(transport)
     sz = _ctypes.c_char_p(0)
     status = _rv.tibrvTransport_GetNetwork(tx, _ctypes.byref(sz))
 
@@ -226,12 +229,12 @@ def tibrvTransport_GetNetwork(transport: tibrvTransport) -> (tibrv_status, str):
 #                const char**        daemonString
 #              );
 #
-_rv.tibrvTransport_GetDaemon.argtypes = [c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
-_rv.tibrvTransport_GetDaemon.restype = c_tibrv_status
+_rv.tibrvTransport_GetDaemon.argtypes = [_c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
+_rv.tibrvTransport_GetDaemon.restype = _c_tibrv_status
 
 def tibrvTransport_GetDaemon(transport: tibrvTransport) -> (tibrv_status, str):
 
-    tx = c_tibrvTransport(transport)
+    tx = _c_tibrvTransport(transport)
     sz = _ctypes.c_char_p(0)
     status = _rv.tibrvTransport_GetDaemon(tx, _ctypes.byref(sz))
 
@@ -244,12 +247,12 @@ def tibrvTransport_GetDaemon(transport: tibrvTransport) -> (tibrv_status, str):
 #                const char*         description
 #              );
 #
-_rv.tibrvTransport_SetDescription.argtypes = [c_tibrvTransport, _ctypes.c_char_p]
-_rv.tibrvTransport_SetDescription.restype = c_tibrv_status
+_rv.tibrvTransport_SetDescription.argtypes = [_c_tibrvTransport, _ctypes.c_char_p]
+_rv.tibrvTransport_SetDescription.restype = _c_tibrv_status
 
 def tibrvTransport_SetDescription(transport: tibrvTransport, description: str) -> tibrv_status:
 
-    tx = c_tibrvTransport(transport)
+    tx = _c_tibrvTransport(transport)
     sz = _cstr(description)
     status = _rv.tibrvTransport_SetDescription(tx, sz)
 
@@ -263,12 +266,12 @@ def tibrvTransport_SetDescription(transport: tibrvTransport, description: str) -
 #                const char**        description
 #              );
 #
-_rv.tibrvTransport_GetDescription.argtypes = [c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
-_rv.tibrvTransport_GetDescription.restype = c_tibrv_status
+_rv.tibrvTransport_GetDescription.argtypes = [_c_tibrvTransport, _ctypes.POINTER(_ctypes.c_char_p)]
+_rv.tibrvTransport_GetDescription.restype = _c_tibrv_status
 
 def tibrvTransport_GetDescription(transport: tibrvTransport) -> (tibrv_status, str):
 
-    tx = c_tibrvTransport(transport)
+    tx = _c_tibrvTransport(transport)
     sz = _ctypes.c_char_p(0)
     status = _rv.tibrvTransport_GetDescription(tx, _ctypes.byref(sz))
 
@@ -281,162 +284,15 @@ def tibrvTransport_GetDescription(transport: tibrvTransport) -> (tibrv_status, s
 #                tibrv_f64           reliability
 #              );
 #
-_rv.tibrvTransport_RequestReliability.argtypes = [c_tibrvTransport, c_tibrv_f64]
-_rv.tibrvTransport_RequestReliability.restype = c_tibrv_status
+_rv.tibrvTransport_RequestReliability.argtypes = [_c_tibrvTransport, _c_tibrv_f64]
+_rv.tibrvTransport_RequestReliability.restype = _c_tibrv_status
 
 def tibrvTransport_RequestReliability(transport:tibrvTransport, reliability: float) -> tibrv_status:
 
-    tx = c_tibrvTransport(transport)
-    n = c_tibrv_f64(reliability)
+    tx = _c_tibrvTransport(transport)
+    n = _c_tibrv_f64(reliability)
     status = _rv.tibrvTransport_RequestReliability(tx, n)
 
     return status
-
-
-class TibrvTx :
-    def __init__(self):
-        self._tx = 0
-        self._err = None
-
-    def __del__(self):
-        self.destroy()
-
-    def id(self):
-        return self._tx
-
-    def create(self, service : str, network:str, daemon:str) -> int :
-        if self._tx != 0:
-            status = TIBRV_ID_IN_USE
-        else:
-            tx = 0
-            status, tx = tibrvTransport_Create(service, network, daemon);
-
-            if status == TIBRV_OK:
-                self._tx = tx
-
-        self._err = TibrvStatus.error(status)
-
-        return status;
-
-    def destroy(self) -> int :
-        if self._tx == 0:
-            status = TIBRV_INVALID_TRANSPORT
-        else:
-            status = tibrvTransport_Destroy(self._tx)
-
-        self._tx = 0
-
-        self._err = TibrvStatus.error(status)
-
-        return status
-
-    @property
-    def description(self) -> str:
-        ret = None
-        status, ret = tibrvTransport_GetDescription(self._tx)
-        self._err = TibrvStatus.error(status)
-
-        return ret
-
-    @description.setter
-    def description(self, sz: str) -> None:
-        status = tibrvTransport_SetDescription(self._tx, sz)
-        self._err = TibrvStatus.error(status)
-
-    @property
-    def service(self) -> str:
-        ret = None
-        status, ret = tibrvTransport_GetService(self._tx)
-        self._err = TibrvStatus.error(status)
-
-        return ret
-
-    @property
-    def network(self) -> str:
-        ret = None
-        status, ret = tibrvTransport_GetNetwork(self._tx)
-        self._err = TibrvStatus.error(status)
-
-        return ret
-
-    @property
-    def daemon(self) -> str:
-        ret = None
-        status, ret = tibrvTransport_GetDaemon(self._tx)
-        self._err = TibrvStatus.error(status)
-
-        return ret
-
-    def inbox(self) -> str:
-        ret = None
-        status, ret = tibrvTransport_CreateInbox(self._tx)
-        self._err = TibrvStatus.error(status)
-
-        return ret
-
-    def reliability(self, reliability: float) -> int:
-        status = tibrvTransport_RequestReliability(self._tx, reliability)
-        self._err = TibrvStatus.error(status)
-
-        return status
-
-    @property
-    def error(self) -> TibrvError :
-        return self._err
-
-    def send(self, msg: TibrvMsg, subj: str=None) -> tibrv_status:
-        if msg is None or type(msg) is not TibrvMsg :
-            status = TIBRV_INVALID_MSG
-        else:
-            if subj is not None:
-                status = tibrvMsg_SetSendSubject(msg.id(), subj)
-                if status != TIBRV_OK:
-                    self._err = TibrvStatus.error(status)
-                    return status
-
-            status = tibrvTransport_Send(self._tx, msg.id())
-
-        self._err = TibrvStatus.error(status)
-
-        return status
-
-    def sendRequest(self, msg: TibrvMsg, timeout : float, subj:str = None) -> (tibrv_status, TibrvMsg):
-        if msg is None or type(msg) is not TibrvMsg:
-            status = TIBRV_INVALID_MSG
-            self._err = TibrvStatus.error(status)
-            return status, None
-
-        if subj is not None:
-            status = tibrvMsg_SetSendSubject(msg.id(), subj)
-            if status != TIBRV_OK:
-                self._err = TibrvStatus.error(status)
-                return status, None
-
-        reply = None
-        status, m = tibrvTransport_SendRequest(self._tx, msg.id(), timeout)
-        if status == TIBRV_OK:
-            reply = TibrvMsg(m)
-
-        self._err = TibrvStatus.error(status)
-
-        return status, reply
-
-    def sendReply(self, msg: TibrvMsg, request: TibrvMsg) -> tibrv_status:
-        if msg is None or type(msg) is not TibrvMsg:
-            status = TIBRV_INVALID_MSG
-            self._err = TibrvStatus.error(status)
-            return status
-
-        if request is None or type(request) is not TibrvMsg:
-            status = TIBRV_INVALID_MSG
-            self._err = TibrvStatus.error(status)
-            return status
-
-        status = tibrvTransport_SendReply(self._tx, msg.id(), request.id())
-
-        self._err = TibrvStatus.error(status)
-
-        return status
-
 
 
