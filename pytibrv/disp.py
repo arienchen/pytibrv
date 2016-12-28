42f653171e8f2f6b118b96f742f0e5acb3c4eeab
Arien Chen <arien.chen@fubon.com>
2016-12-28 14:46:09 +0800

    validate mandatory parameters

--- pytibrv/disp.py
+++ pytibrv/disp.py
@@ -21,6 +21,7 @@
 #   CREATED
 #
 import ctypes as _ctypes
+
 from .types import tibrvDispatcher, tibrvDispatchable, tibrv_status, \
                    TIBRV_WAIT_FOREVER, TIBRV_NO_WAIT
 
@@ -28,6 +29,7 @@ from .api import _rv, _cstr, _pystr, \
                  _c_tibrv_status, _c_tibrvDispatcher, _c_tibrvDispatchable, \
                  _c_tibrv_f64
 
+from .status import TIBRV_INVALID_DISPATCHER, TIBRV_INVALID_ARG, TIBRV_INVALID_DISPATCHABLE
 
 ##
 # tibrv/disp.h
@@ -45,9 +47,24 @@ _rv.tibrvDispatcher_CreateEx.restype = _c_tibrv_status
 def tibrvDispatcher_Create(dispatchable: tibrvDispatchable, idleTimeout: float = TIBRV_WAIT_FOREVER) \
                           -> (tibrv_status, tibrvDispatcher):
 
+    if dispatchable is None or dispatchable == 0:
+        return TIBRV_INVALID_DISPATCHABLE
+
+    if idleTimeout is None:
+        return TIBRV_INVALID_ARG
+
     disp = _c_tibrvDispatcher(0)
-    que  = _c_tibrvDispatchable(dispatchable)
-    t = _c_tibrv_f64(idleTimeout)
+
+    try:
+        que  = _c_tibrvDispatchable(dispatchable)
+    except:
+        return TIBRV_INVALID_DISPATCHABLE
+
+    try:
+        t = _c_tibrv_f64(idleTimeout)
+    except:
+        return TIBRV_INVALID_ARG
+
     status = _rv.tibrvDispatcher_CreateEx(_ctypes.byref(disp), que, t)
 
     return status, disp.value
@@ -61,9 +78,16 @@ _rv.tibrvDispatcher_Join.argtypes = [_c_tibrvDispatcher]
 _rv.tibrvDispatcher_Join.restype = _c_tibrv_status
 
 
-def tibrvDispatcher_Join(dispatcher:tibrvDispatcher) -> tibrv_status:
+def tibrvDispatcher_Join(dispatcher: tibrvDispatcher) -> tibrv_status:
 
-    disp = _c_tibrvDispatcher(dispatcher)
+    if dispatcher is None or dispatcher == 0:
+        return TIBRV_INVALID_DISPATCHER
+
+    try:
+        disp = _c_tibrvDispatcher(dispatcher)
+    except:
+        return TIBRV_INVALID_DISPATCHER
+
     status = _rv.tibrvDispatcher_Join(disp)
     return status
 
@@ -77,9 +101,16 @@ def tibrvDispatcher_Join(dispatcher:tibrvDispatcher) -> tibrv_status:
 _rv.tibrvDispatcher_Destroy.argtypes = [_c_tibrvDispatcher]
 _rv.tibrvDispatcher_Destroy.restype = _c_tibrv_status
 
-def tibrvDispatcher_Destroy(dispatcher:tibrvDispatcher) -> tibrv_status:
+def tibrvDispatcher_Destroy(dispatcher: tibrvDispatcher) -> tibrv_status:
 
-    disp = _c_tibrvDispatcher(dispatcher)
+    if dispatcher is None or dispatcher == 0:
+        return TIBRV_INVALID_DISPATCHER
+
+    try:
+        disp = _c_tibrvDispatcher(dispatcher)
+    except:
+        return TIBRV_INVALID_DISPATCHER
+
     status = _rv.tibrvDispatcher_Destroy(disp)
 
     return status
@@ -95,10 +126,21 @@ def tibrvDispatcher_Destroy(dispatcher:tibrvDispatcher) -> tibrv_status:
 _rv.tibrvDispatcher_SetName.argtypes = [_c_tibrvDispatcher, _ctypes.c_char_p]
 _rv.tibrvDispatcher_SetName.restype = _c_tibrv_status
 
-def tibrvDispatcher_SetName(dispatcher:tibrvDispatcher, dispatchName:str) -> tibrv_status:
+def tibrvDispatcher_SetName(dispatcher: tibrvDispatcher, dispatchName: str) -> tibrv_status:
 
-    disp = _c_tibrvDispatcher(dispatcher)
-    sz = _cstr(dispatchName)
+    if dispatcher is None or dispatcher == 0:
+        return TIBRV_INVALID_DISPATCHER
+
+    try:
+        disp = _c_tibrvDispatcher(dispatcher)
+    except:
+        return TIBRV_INVALID_DISPATCHER
+
+    try:
+        sz = _cstr(dispatchName)
+    except:
+        return TIBRV_INVALID_ARG
+
     status = _rv.tibrvDispatcher_SetName(disp, sz)
 
     return status
@@ -112,9 +154,16 @@ def tibrvDispatcher_SetName(dispatcher:tibrvDispatcher, dispatchName:str) -> tib
 _rv.tibrvDispatcher_GetName.argtypes = [_c_tibrvDispatcher, _ctypes.POINTER(_ctypes.c_char_p)]
 _rv.tibrvDispatcher_GetName.restype = _c_tibrv_status
 
-def tibrvDispatcher_GetName(dispatcher:tibrvDispatcher) -> (tibrv_status, str):
+def tibrvDispatcher_GetName(dispatcher: tibrvDispatcher) -> (tibrv_status, str):
 
-    disp = _c_tibrvDispatcher(dispatcher)
+    if dispatcher is None or dispatcher == 0:
+        return TIBRV_INVALID_DISPATCHER
+
+    try:
+        disp = _c_tibrvDispatcher(dispatcher)
+    except:
+        return TIBRV_INVALID_DISPATCHER
+
     sz = _ctypes.c_char_p()
     status = _rv.tibrvDispatcher_GetName(disp, _ctypes.byref(sz))
 
